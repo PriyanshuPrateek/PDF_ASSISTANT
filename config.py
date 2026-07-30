cf7137ad818b2379f71602eed7b4f713acfa6db7
Priyanshu Prateek <prateekpriyanshu3@gmail.com>
2026-07-31 01:51:43 +0530

    model name change and requirement version added

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 groq_api_key = os.getenv("GROQ_API_KEY")
-model_name = "meta-llama/llama-4-scout-17b-16e-instruct"
+model_name = "openai/gpt-oss-20b"
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
